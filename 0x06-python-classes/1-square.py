8f584d4ffc712b2282e1da58bc2d14f303cd45de
LinusConradM <linusconradm@gmail.com>
2022-10-11 08:59:58 -0700

    Program update

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''Defining a class Square Defined by size'''
+
+
+class Square:
+    '''Defines a square'''
+
+    def __init__(self, size):
+        '''
+        Initializes a square
+        Args:
+            size: size of the square
+        '''
+        self.__size = sizeX
